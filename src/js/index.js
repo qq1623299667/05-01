b8dfd6dbf549386d7d10294083a5cf92db31e820
石佳 <jia.shi@casstime.com>
2020-08-26 15:26:38 +0800

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,21 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-ReactDOM.render(
-    <h1>hello world...</h1>,
-    document.getElementById("example")
-);
+var ExampleApplication = React.createClass({
+    render:function () {
+        var elapsed = Math.round(this.props.elapsed / 100);
+        var seconds = elapsed / 10 + (elapsed % 10 ?'':'.0');
+        var message = 'React has bean successfully running for '+seconds+' seconds.';
+        return React.DOM.p(null,message);
+    }
+});
+
+var ExampleApplicationFactory = React.createFactory(ExampleApplication);
+
+var start = new Date().getTime();
+setInterval(function () {
+    ReactDOM.render(
+        ExampleApplicationFactory({elapsed:new Date().getTime()-start}),
+        document.getElementById("example")
+    );
+},50);
