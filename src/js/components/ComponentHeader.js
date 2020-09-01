eac9623aa9693abded2ab053576fd73a1da80d32
石佳 <jia.shi@casstime.com>
2020-09-01 11:27:42 +0800

    css内联样式表达式

--- src/js/components/ComponentHeader.js
+++ src/js/components/ComponentHeader.js
@@ -2,19 +2,30 @@ import React from 'react';
 
 // 给外部使用的组件需要加上 export default
 export default class ComponentHeader extends React.Component{
+    constructor() {
+        super();
+        this.state={
+            miniHeader : false
+        }
+    }
+    switchHeader(){
+        this.setState({
+            miniHeader:!this.state.miniHeader
+        });
+    }
     render() {
         const styleComponentHeader = {
             header:{
                 backgroundColor:"#333333", // react中的css的样式
                 color:"#FFFFFF",
-                "padding-top":"15px",  // 原生css的样式
-                paddingBottom:"15px"
+                "padding-top":this.state.miniHeader?"3px":"15px",  // 原生css的样式
+                paddingBottom:this.state.miniHeader?"3px":"15px"
             },
             // 还可以定义其他的样式
         };
 
         return (
-            <header style={styleComponentHeader.header} className="smallFontSize">
+            <header style={styleComponentHeader.header} className="smallFontSize" onClick={this.switchHeader.bind(this)}>
                 <h1>这里是头部</h1>
             </header>
         )
