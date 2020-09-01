13e201bb5b67e4158041c0b9369ebb9830c546c4
石佳 <jia.shi@casstime.com>
2020-09-01 10:39:01 +0800

    js代码里面设置css样式

--- src/js/components/ComponentHeader.js
+++ src/js/components/ComponentHeader.js
@@ -3,8 +3,18 @@ import React from 'react';
 // 给外部使用的组件需要加上 export default
 export default class ComponentHeader extends React.Component{
     render() {
+        const styleComponentHeader = {
+            header:{
+                backgroundColor:"#333333", // react中的css的样式
+                color:"#FFFFFF",
+                "padding-top":"15px",  // 原生css的样式
+                paddingBottom:"15px"
+            },
+            // 还可以定义其他的样式
+        };
+
         return (
-            <header>
+            <header style={styleComponentHeader.header}>
                 <h1>这里是头部</h1>
             </header>
         )
